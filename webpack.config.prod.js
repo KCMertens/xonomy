9101e0a97d9a1ccdda1fcf80deca38e4113033b2
KCMertens <koen.mmm@gmail.com>
2021-08-23 15:35:40 +0200

    Initial commit - build pipeline works, xonomy code does not (yet)

---
+++ webpack.config.prod.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+	entry: {
+		'xonomy': './src/xonomy.ts',
+	},
+	module: {
+		rules: [{
+			test: /\.tsx?$/,
+			exclude: /node_modules/,
+			loader: 'ts-loader',
+		}]
+	},
+	output: {
+		filename: '[name].js',
+		// filename: '[name].js',
+		// Path on disk for output file
+		path: path.resolve(__dirname, 'dist'),
+		// Path in webpack-dev-server for compiled files (has priority over disk files in case both exist)
+		publicPath: '/dist/',
+		clean: true, // clean previous outputs prior to compiling
+		library: ['Xomomy']
+	},
+	resolve: {
+		extensions: ['.js', '.ts'], // enable autocompleting .ts and .js extensions when using import '...'
+		alias: {
+			// Enable importing source files by their absolute path by prefixing with "@/"
+			// Note: this also requires typescript to be able to find the imports (though it doesn't use them other than for type checking), see tsconfig.json
+			"@": path.join(__dirname, "src"),
+		}
+	}
+};
